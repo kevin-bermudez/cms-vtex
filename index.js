946c51d33b212e988f2620da6dff0843f7cc9100
kevin <kbermudes@chefcompany.co>
2019-09-09 23:27:03 -0500

    Adapatod ok

--- index.js
+++ index.js
@@ -4,24 +4,20 @@ const cheerio = require('cheerio');
 const entities = require('html-entities').XmlEntities;
 const fs = require('fs');
 const path = require('path')
-if(__dirname == '/'){
-	const CMSVtex_website = require('./libs/CMSVtex_website');
-	const CMSVtex_folder = require('./libs/CMSVtex_folder');
-	const CMSVtex_layout = require('./libs/CMSVtex_layout');
-	const CMSVtex_custom_elements = require('./libs/CMSVtex_custom_elements');
-	const CMSVtex_template = require('./libs/CMSVtex_template');
-	const CMSVtex_file = require('./libs/CMSVtex_file');
-	const CMSVtex_authentication = require('./libs/CMSVtex_authentication');
+let base_dir = path.join(__dirname)
+
+if(__dirname.trim() == '/'){
+	base_dir = './'
+	
 }
-else{
-	const CMSVtex_website = require(path.join(__dirname,'libs/CMSVtex_website'));
-	const CMSVtex_folder = require(path.join(__dirname,'libs/CMSVtex_folder'));
-	const CMSVtex_layout = require(path.join(__dirname,'libs/CMSVtex_layout'));
-	const CMSVtex_custom_elements = require(path.join(__dirname,'libs/CMSVtex_custom_elements'));
-	const CMSVtex_template = require(path.join(__dirname,'libs/CMSVtex_template'));
-	const CMSVtex_file = require(path.join(__dirname,'libs/CMSVtex_file'));
-	const CMSVtex_authentication = require(path.join(__dirname,'libs/CMSVtex_authentication'));
-}
+
+const CMSVtex_website = require(base_dir + 'libs/CMSVtex_website');
+const CMSVtex_folder = require(base_dir + 'libs/CMSVtex_folder');
+const CMSVtex_layout = require(base_dir + 'libs/CMSVtex_layout');
+const CMSVtex_custom_elements = require(base_dir + 'libs/CMSVtex_custom_elements');
+const CMSVtex_template = require(base_dir + 'libs/CMSVtex_template');
+const CMSVtex_file = require(base_dir + 'libs/CMSVtex_file');
+const CMSVtex_authentication = require(base_dir + 'libs/CMSVtex_authentication');
 
 module.exports = function(){
 	return {
