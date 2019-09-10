9c95b440b55f01f501f8f90dcfa2c9d7eb2fe7dc
kevin <kbermudes@chefcompany.co>
2019-09-09 23:12:23 -0500

    Index para funcionar con babel

--- index.js
+++ index.js
@@ -4,13 +4,24 @@ const cheerio = require('cheerio');
 const entities = require('html-entities').XmlEntities;
 const fs = require('fs');
 const path = require('path')
-const CMSVtex_website = require(path.join(__dirname,'libs/CMSVtex_website'));
-const CMSVtex_folder = require(path.join(__dirname,'libs/CMSVtex_folder'));
-const CMSVtex_layout = require(path.join(__dirname,'libs/CMSVtex_layout'));
-const CMSVtex_custom_elements = require(path.join(__dirname,'libs/CMSVtex_custom_elements'));
-const CMSVtex_template = require(path.join(__dirname,'libs/CMSVtex_template'));
-const CMSVtex_file = require(path.join(__dirname,'libs/CMSVtex_file'));
-const CMSVtex_authentication = require(path.join(__dirname,'libs/CMSVtex_authentication'));
+if(__dirname == '/'){
+	const CMSVtex_website = require('./libs/CMSVtex_website');
+	const CMSVtex_folder = require('./libs/CMSVtex_folder');
+	const CMSVtex_layout = require('./libs/CMSVtex_layout');
+	const CMSVtex_custom_elements = require('./libs/CMSVtex_custom_elements');
+	const CMSVtex_template = require('./libs/CMSVtex_template');
+	const CMSVtex_file = require('./libs/CMSVtex_file');
+	const CMSVtex_authentication = require('./libs/CMSVtex_authentication');
+}
+else{
+	const CMSVtex_website = require(path.join(__dirname,'libs/CMSVtex_website'));
+	const CMSVtex_folder = require(path.join(__dirname,'libs/CMSVtex_folder'));
+	const CMSVtex_layout = require(path.join(__dirname,'libs/CMSVtex_layout'));
+	const CMSVtex_custom_elements = require(path.join(__dirname,'libs/CMSVtex_custom_elements'));
+	const CMSVtex_template = require(path.join(__dirname,'libs/CMSVtex_template'));
+	const CMSVtex_file = require(path.join(__dirname,'libs/CMSVtex_file'));
+	const CMSVtex_authentication = require(path.join(__dirname,'libs/CMSVtex_authentication'));
+}
 
 module.exports = function(){
 	return {
